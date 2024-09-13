d9cad8c6c530d549bebd2ab66b611154a8c88e6a
ZeusExx <nicolasouzaxx@gmail.com>
2024-09-13 16:13:37 -0300

    V 3.0

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const expressLayouts = require('express-ejs-layouts');
-const userRoutes = require('./routes/userRoutes');
 const indexRoutes = require('./routes/indexRoutes');
-const produtoRouter = require('./routes/produtoRoutes');
+const userRoutes = require('./routes/userRoutes');
+const produtoRoutes = require('./routes/produtoRoutes');
+const categoriaRoutes = require('./routes/categoriaRoutes');
+const vendasRoutes = require('./routes/vendasRoutes');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -16,9 +19,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
-app.use('/',  indexRoutes)
+app.use('/', indexRoutes);
 app.use('/users', userRoutes);
-app.use('/produtos', produtoRouter);
+app.use('/produtos', produtoRoutes);
+app.use('/categorias', categoriaRoutes);
+app.use('/vendas', vendasRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
--- database.sql
+++ database.sql
@@ -1,23 +1,36 @@
+CREATE DATABASE CRUD;
+
 USE CRUD;
 
-drop table produtos;
-drop table categoria;
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role ENUM('admin', 'user') NOT NULL
+);
+
+CREATE TABLE categorias (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL
+);
 
 CREATE TABLE produtos (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(100),
-    category int NOT NULL,
-    price VARCHAR(255) NOT NULL,
-    role ENUM('admin', 'user') NOT NULL
-);
-CREATE TABLE categoria (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(100)
+    nome VARCHAR(255) NOT NULL,
+    descricao TEXT NOT NULL,
+    preco DECIMAL(10,2) NOT NULL,
+    quantidade INT NOT NULL,
+    categoria INT NOT NULL,
+    FOREIGN KEY (categoria) REFERENCES categorias(id)
 );
 
-ALTER TABLE produtos
-ADD CONSTRAINT fk_categoria
-FOREIGN KEY (category) REFERENCES categoria(id);
-
-insert into categoria(nome) values ("esportes");
-select * from categoria;
+CREATE TABLE vendas (
+    id INT PRIMARY KEY,
+    id_user INT,
+    id_produto INT,
+    quantidade INT,
+    valor_total FLOAT,
+    data_venda DATE,
+    FOREIGN KEY (id_user) REFERENCES users(id),
+    FOREIGN KEY (id_produto) REFERENCES produtos(id)
+);
